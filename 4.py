f203f54cf05c125ce5119aef2b6e18ed5bf71af2
kew2023 <58667109+kew2023@users.noreply.github.com>
2023-08-03 16:38:57 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,30 @@
+def p(n):
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
+
+a1 = '228'
+a2 = '777'
+alf = '0123456789'
+arr = []
+
+for n in alf:
+    
+    a = int('228777'+ n)
+    if p(a): arr.append(a)
+
+    if n != 0:
+        a = int(n + '228777')
+        if p(a): arr.append(a)
+
+    a = int('22877'+n+'7')
+    if p(a): arr.append(a)
+
+    a = int('2287'+n+'77')
+    if p(a): arr.append(a)
+    
+    a = int('228'+n+'777')
+    if p(a): arr.append(a)
+
+print(max(sorted(arr))+100000)
