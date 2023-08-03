00429819232e84535a40ea64d600bd2b2923b249
kew2023 <58667109+kew2023@users.noreply.github.com>
2023-08-03 16:47:33 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 def dell(x,a):
     return x % a == 0
 
-for a in range(1,200):
+for a in range(1,1000):
     for x in range(1,1000):
         f = ((not dell(72,x)) or (not dell(120,x))) or (a-x > 100)
         if f == 0:
